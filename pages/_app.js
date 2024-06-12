7f6114ed4c786d62a583cc4c018068d5563517bc
Various5 <varous555@gmail.com>
2024-06-12 16:42:16 +0200

    a

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
