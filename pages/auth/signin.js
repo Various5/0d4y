c9e62abc2fee0273f24fba2996649c87a6f3a33b
Various5 <varous555@gmail.com>
2024-06-19 22:59:10 +0200

    a

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,12 +1,12 @@
 import { signIn } from 'next-auth/client';
-import styles from '../../styles/signin.module.css';
+import styles from './signin.module.css';
 
 const SignIn = () => {
   return (
     <div className={styles.container}>
       <h1>Sign In</h1>
-      <button onClick={() => signIn('google')}>Sign in with Google</button>
-      <button onClick={() => signIn('github')}>Sign in with GitHub</button>
+      <button className={styles.signInButton} onClick={() => signIn('google')}>Sign in with Google</button>
+      <button className={styles.signInButton} onClick={() => signIn('github')}>Sign in with GitHub</button>
     </div>
   );
 };
--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,12 +1,12 @@
 import { signIn } from 'next-auth/client';
-import styles from '../../styles/signup.module.css';
+import styles from './signup.module.css';
 
 const SignUp = () => {
   return (
     <div className={styles.container}>
       <h1>Sign Up</h1>
-      <button onClick={() => signIn('google')}>Sign up with Google</button>
-      <button onClick={() => signIn('github')}>Sign up with GitHub</button>
+      <button className={styles.signUpButton} onClick={() => signIn('google')}>Sign up with Google</button>
+      <button className={styles.signUpButton} onClick={() => signIn('github')}>Sign up with GitHub</button>
     </div>
   );
 };
