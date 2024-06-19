b0e4bd86a8fff1939c51d896f114fe134bb55a50
Various5 <varous555@gmail.com>
2024-06-19 21:52:06 +0200

    a

--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const About = () => {
   return (
-    <Div>
+    <div>
       <h1>About Us</h1>
       <p>This is the about page with more information about our website.</p>
-    </Div>
+    </div>
   );
 };
 
