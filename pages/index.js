0d06f9c2e331cec6fc6e56f57898b22702f4401b
Various5 <varous555@gmail.com>
2024-06-19 21:47:56 +0200

    a

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 const Home = () => {
   return (
-    <Div>
+    <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is an example of a modern, dynamic, and responsive web page.</p>
-    </Div>
+    </div>
   );
 };
 
