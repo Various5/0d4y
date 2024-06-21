28d8b04cfe41ba6aa6f3ab7636ce1e1ce3987871
Various5 <varous555@gmail.com>
2024-06-21 14:15:12 +0200

    a

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Blog from './blog';
+import Blog from './Blog';
 import CreatePost from '../components/CreatePost';
 import NetworkTools from './network-tools';
 import DomainTools from './domain-tools';
